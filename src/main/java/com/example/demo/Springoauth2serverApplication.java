64485b732e922531ec816384ddcd5837e201fa82
Prashant <yesprashant83@gmail.com>
2022-07-07 10:35:01 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/Springoauth2serverApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springoauth2serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springoauth2serverApplication.class, args);
+	}
+
+}
